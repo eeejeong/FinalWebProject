88468f2ce03fbc3773db236e5e3c0cd20c973db4
user23 <ennea9on@gmail.com>
2019-10-24 13:00:02 +0900

    의약품 관리 컨트롤러 추가

---
+++ src/main/java/com/medirone/web/controller/ItemManagementController.java
@@ -0,0 +1,10 @@
+package com.medirone.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ItemManagementController {
+	private static final Logger logger = LoggerFactory.getLogger(ItemManagementController.class);
+	
+
+}
