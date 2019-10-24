3db7db4e7236ce893ab2474b43e2143a2bb35ced
user4 <user4@iot.com>
2019-10-24 13:17:54 +0900

    ...

--- src/main/java/com/medirone/web/controller/ItemManagementController.java
+++ src/main/java/com/medirone/web/controller/ItemManagementController.java
@@ -2,9 +2,9 @@ package com.medirone.web.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 
 public class ItemManagementController {
 	private static final Logger logger = LoggerFactory.getLogger(ItemManagementController.class);
 	
-
 }
